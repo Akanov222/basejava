07fbe2fce73acc7087449ae7b8117cd82e7780ed
Akanov222 <akanov.gazproekt@gmail.com>
2024-02-05 22:32:12 +0300

    Project change with exeption for the first check
    - modified:   src/ArrayStorage.java
    - modified:   src/MainArray.java
    - modified:   src/MainTestArrayStorage.java
    - modified:   src/Resume.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,57 +1,45 @@
 import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 
 public class ArrayStorage {
-    public Resume[] storage = new Resume[10000];
-    private int count = 0;
-    private Resume tempOneResume = null;
-
-    public void clear() {
+    Resume[] storage = new Resume[4];
+    int count = 0;
+    Resume tempResume = null;
+    
+    void clear() {
         Arrays.fill(storage, null);
     }
-
-    public void save(Resume r) {
+    void save(Resume r) {
         storage[count] = r;
         count++;
     }
-
-    public Resume get(String uuid) {
-        findUuid(uuid);
-        return tempOneResume;
-    }
-
-    public void delete(String uuid) {
+    Resume get(String uuid) {
+        tempResume = null;
         for (int i = 0; i < storage.length; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
+                tempResume = storage[i];
+            }
+        }
+        return tempResume;
+    }
+    void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
             }
         }
         deleteNull();
     }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
+    Resume[] getAll() {
         deleteNull();
         return Arrays.copyOf(storage, storage.length);
     }
-
     int size() {
         return count;
     }
-    private void findUuid(String uuid) {
-        for (Resume resume : storage) {
-            if (resume.getUuid().equals(uuid)) {
-                tempOneResume = resume;
-                break;
-            }
-        }
-    }
-    private void deleteNull() {
+    void deleteNull() {
         Resume[] tempResume = new Resume[storage.length - 1];
         count = 0;
         for (int i = 0; i < storage.length; i++) {
@@ -63,4 +51,3 @@ public class ArrayStorage {
         }
     }
 }
-
--- src/Resume.java
+++ src/Resume.java
@@ -4,20 +4,10 @@
 public class Resume {
 
     // Unique identifier
-    private String uuid;
+    String uuid;
 
     @Override
     public String toString() {
         return uuid;
-    }
-    public String getUuid() {
-        try {
-            return uuid;
-        } catch (NullPointerException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 }
