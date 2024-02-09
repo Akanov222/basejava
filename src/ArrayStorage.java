32235346d4a5886d24d7e68ca2e9863066479c73
Akanov222 <akanov.gazproekt@gmail.com>
2024-02-09 21:15:30 +0300

    Changes to be committed:
    - modified:   ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -7,15 +8,15 @@ public class ArrayStorage {
     int count;
 
     void clear() {
-        Arrays.fill(storage,0, count - 1, null);
+        Arrays.fill(storage, 0, count - 1, null);
         count = 0;
     }
-    
+
     void save(Resume r) {
         storage[count] = r;
         count++;
     }
-    
+
     Resume get(String uuid) {
         int index = findIndex(uuid);
         if (index == -1) {
@@ -24,24 +25,24 @@ public class ArrayStorage {
         }
         return storage[index];
     }
-    
+
     void delete(String uuid) {
         int index = findIndex(uuid);
         storage[index] = storage[count - 1];
         storage[count - 1] = null;
         count--;
     }
-    
+
     Resume[] getAll() {
         Resume[] resumes = new Resume[count];
         System.arraycopy(storage, 0, resumes, 0, count);
         return resumes;
     }
-    
+
     int size() {
         return count;
     }
-    
+
     int findIndex(String uuid) {
         for (int i = 0; i < count; i++) {
             if (storage[i].uuid.equals(uuid)) {
