7689b6efccae57138d47a45353ca68418c86cd4f
Akanov222 <146616561+Akanov222@users.noreply.github.com>
2024-02-15 01:16:16 +0300

    Update ArrayStorage.java_Lesson 2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,59 +4,78 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int count;
+	Resume[] storage = new Resume[4];
+	private int size = 0;
 
-    void clear() {
-        if(count != 0 ) {
-            Arrays.fill(storage, 0, count, null);
-            count = 0;
-        }
-    }
+	public void clear() {
+		if (size != 0) {
+			Arrays.fill(storage, 0, size, null);
+			size = 0;
+		}
+	}
 
-    void save(Resume r) {
-        storage[count] = r;
-        count++;
-    }
+	public void save(Resume resume) {
+		if (findIndex(resume) == null) {
+			storage[size] = resume;
+			size++;
+		} else {
+			System.out.println("This resume is already in the repository");
+		}
+	}
 
-    Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume is not found");
-            return null;
-        }
-        return storage[index];
-    }
+	public void update(Resume resume) {
+		Integer index = findIndex(resume);
+		if (index != null) {
+			storage[index] = resume;
+		} else {
+			System.out.println("This resume is not found");
+		}
+	}
 
-    void delete(String uuid) {
-        if(count != 0 ) {
-            int index = findIndex(uuid);
-            if (index == -1) {
-            System.out.println("Resume is not found");
-            return null;
-            }
-            storage[index] = storage[count - 1];
-            storage[count - 1] = null;
-            count--;
-        }
-    }
+	public Resume get(String uuid) {
+		Integer index = findIndex(uuid);
+		if (index == null) {
+			System.out.println("Resume is not found");
+			return null;
+		}
+		return storage[index];
+	}
 
-    Resume[] getAll() {
-        Resume[] resumes = new Resume[count];
-        System.arraycopy(storage, 0, resumes, 0, count);
-        return resumes;
-    }
+	public void delete(String uuid) {
+		Integer index = findIndex(uuid);
+		if (index == null) {
+			System.out.println("Resume is not found");
+		}
+		storage[index] = storage[size - 1];
+		storage[size - 1] = null;
+		size--;
+	}
 
-    int size() {
-        return count;
-    }
+	public Resume[] getAll() {
+		Resume[] resumes = new Resume[size];
+		System.arraycopy(storage, 0, resumes, 0, size);
+		return resumes;
+	}
 
-    int findIndex(String uuid) {
-        for (int i = 0; i < count; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
+	public int size() {
+		return size;
+	}
+
+	private Integer findIndex(String uuid) {
+		for (int i = 0; i < size; i++) {
+			if (storage[i].getUuid().equals(uuid)) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+	private Integer findIndex(Resume resume) {
+		for (int i = 0; i < size; i++) {
+			if (resume.equals(storage[i])) {
+				return i;
+			}
+		}
+		return null;
+	}
 }
