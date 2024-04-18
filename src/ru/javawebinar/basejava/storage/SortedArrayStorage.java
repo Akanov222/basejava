7ecacc9e6fed87e8fa8a9b9955a8b99abbc65f8b
Akanov222 <146616561+Akanov222@users.noreply.github.com>
2024-04-18 08:09:07 +0300

    Update SortedArrayStorage.java_Lesson3

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -1,38 +1,35 @@
 package ru.javawebinar.basejava.storage;
 
+import ru.javawebinar.basejava.model.Resume;
+
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-
-	@Override
-	protected int findIndex(String uuid) {
-		Resume searchKey = new Resume();
-		searchKey.saveUuid(uuid);
-		return Arrays.binarySearch(storage, 0, size, searchKey);
-	}
-
 	@Override
 	public void clear() {
-
-	}
-
-	@Override
-	public void save(Resume resume) {
-
 	}
 
 	@Override
 	public void update(Resume resume) {
+	}
 
+	@Override
+	public void save(Resume resume) {
 	}
 
 	@Override
 	public void delete(String uuid) {
-
 	}
 
 	@Override
 	public Resume[] getAll() {
 		return new Resume[0];
+	}
+
+	@Override
+	protected int getIndex(String uuid) {
+		Resume searchKey = new Resume();
+		searchKey.setUuid(uuid);
+		return Arrays.binarySearch(storage, 0, size, searchKey);
 	}
 }
