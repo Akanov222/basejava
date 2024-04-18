33f7c14127b5ed052598c7896c3fc0513d50cd6f
Akanov222 <146616561+Akanov222@users.noreply.github.com>
2024-04-18 08:07:54 +0300

    Update AbstractArrayStorage.java_Lesson3

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -1,28 +1,29 @@
 package ru.javawebinar.basejava.storage;
 
-import java.util.Arrays;
+import ru.javawebinar.basejava.model.Resume;
 
 /**
  * Array based storage for Resumes
  */
 public abstract class AbstractArrayStorage implements Storage {
 	protected static final int STORAGE_LIMIT = 10000;
-	protected final Resume[] storage = new Resume[STORAGE_LIMIT];
-	protected int index;
+
+	protected /*final*/ Resume[] storage = new Resume[STORAGE_LIMIT];
 	protected int size;
+//	protected int index;
 
 	public  int size() {
 		return size;
 	}
 
 	public Resume get(String uuid) {
-		index = findIndex(uuid);
+		int index = getIndex(uuid);
 		if (index == -1) {
-			System.out.println("Resume is not found");
+			System.out.println("Resume " + uuid + " not exist");
 			return null;
 		}
 		return storage[index];
 	}
 
-	protected abstract int findIndex(String uuid);
+	protected abstract int getIndex(String uuid);
 }
